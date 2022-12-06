527488c9b7be4216c511ef3fed5c9d940cbed980
Andrew <drewbusi123@gmail.com>
2022-12-05 21:11:00 -0500

    Routes started, styling started, boilerplate stuff

--- client/App.js
+++ client/App.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './Styles/global.css'
+import Home from './Components/Home';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div id='app'>
-                <h1 id='title'>Napkin math!</h1>
-            </div>
-        )
-    }
+function App() {
+    //where all routes will exist to each step/mobile page
+    return (  
+        <div className='appBody'>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                {/* <Route path='/Signup' element={} /> */}
+                {/* <Route path='/AddUsers' element={} /> */}
+                {/* <Route path='/ReceiptUpload' element={} /> */}
+                {/* <Route path='/ItemSelector' element={} /> */}
+                {/* <Route path='/TipAmount' element={} /> */}
+                {/* <Route path='/FinalDisplay' element={} /> */}
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Home() {
+    return ( 
+        <div>
+            <div>need to place image here</div>
+            <div>Welcome to NapkinMath!</div>
+            <div>use material ui buttons for Login & Signup</div>
+        </div> 
+    );
+}
+
+export default Home;
