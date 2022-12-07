b6c0e5a5c9936633151484ee2954b85e98549ec4
Jonathan Chen <jonathanchen@macbook-air.myfiosgateway.com>
2022-12-07 11:01:07 -0500

    Styled Home screen and Login screen

--- client/Components/Home.js
+++ client/Components/Home.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import logo from "../Assets/napkinmath.png"
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../Assets/napkinmath.png';
+import '../Styles/Home.css';
 
 function Home() {
-    return ( 
-        <div>
-            <div>Welcome to NapkinMath!</div>
-            <img src = {logo}/>
-            <Link to='/AddUsers'>
-                <button>add users</button>
-            </Link>
-        </div> 
-    );
+  const location = useLocation();
+  const { user } = location.state;
+  console.log(location.state);
+
+  return (
+    <div className='homePage'>
+      <img src={logo} />
+      <div className='welcomeDiv'>
+        <h1 className='welcomeHeader'>
+          Welcome <br></br> {user.given_name}!
+        </h1>
+        <Link to='/AddUsers' className='addusers'>
+          Get Started!
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
